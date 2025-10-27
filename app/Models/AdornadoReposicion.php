a9bd186293993c496802a0bafbfc1197519ce2d7
Bitlocked8 <162711412+Bitlocked8@users.noreply.github.com>
2025-10-27 10:42:31 -0400

    45

---
+++ app/Models/AdornadoReposicion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdornadoReposicion extends Model
+{
+    /** @use HasFactory<\Database\Factories\AdornadoReposicionFactory> */
+    use HasFactory;
+}
